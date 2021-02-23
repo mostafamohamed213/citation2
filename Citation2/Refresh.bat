a27a03574ee261b49790d817175d0e25c6a8f7d6
mostafamohamed213 <64440025+mostafamohamed213@users.noreply.github.com>
2021-02-24 00:32:17 +0200

    demo

--- Citation2/Refresh.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Refreshing Citations for main project...
-cd C:\Users\MostafaAli\Documents\Citation2\Citations
-dotnet ef dbcontext scaffold "Host=localhost;Database=citations;Username=citation_user;Password=citation_user" Npgsql.EntityFrameworkCore.PostgreSQL --schema core -o Models -c CitationContext -f
-echo Done
